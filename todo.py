c5205683e52d476d05b8a32ed3cb303170004903
vanstrouble <vanstrouble@proton.me>
2023-08-16 21:46:22 -0600

    Code reformatted

--- todo.py
+++ todo.py
@@ -2,18 +2,21 @@ from sqlalchemy import create_engine, Column, Integer, String, Date, Boolean
 from sqlalchemy.orm import sessionmaker, declarative_base
 from datetime import datetime
 
-engine = create_engine('sqlite:///todo.db')
+# Create databse
+engine = create_engine("sqlite:///todo.db")
 Base = declarative_base()
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
+
+# Create task table
 class Task(Base):
-    __tablename__ = 'tasks'
+    __tablename__ = "tasks"
 
     id = Column(Integer, primary_key=True, autoincrement=True)
-    name_task = Column(String, default='empty task')
-    details = Column(String, default='empty task details')
+    name_task = Column(String, default="empty task")
+    details = Column(String, default="empty task details")
     created_at = Column(Date, default=datetime.today().date())
     limit_date = Column(Date, default=datetime.today().date())
     status = Column(Boolean, default=False)
@@ -21,8 +24,14 @@ class Task(Base):
     def __repr__(self) -> str:
         return f"Task(id={self.id!r}, name_task={self.name_task!r}, details={self.details!r}, created={self.created_at!r}, limit_date={self.limit_date!r}, status={self.status!r})"
 
-Base.metadata.create_all(engine)
 
-new_task = Task(name_task='Add new task to todo', details='IDK, just making a DB', limit_date=datetime(2020, 8, 20).date())
+# Create all
+Base.metadata.create_all(engine)
+# Add new row
+new_task = Task(
+    name_task="Add new task to todo",
+    details="IDK, just making a DB",
+    limit_date=datetime(2020, 8, 20).date(),
+)
 session.add(new_task)
-session.commit()
+session.commit()  # Consolidate everything
