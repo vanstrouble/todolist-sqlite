bf9c81e53d25c8bda1d082dca4e1302bd361cd60
vanstrouble <vanstrouble@proton.me>
2023-08-16 20:48:55 -0600

    Create table

--- todo.py
+++ todo.py
@@ -6,3 +6,15 @@ from datetime import datetime
 # Create database
 engine = create_engine('sqlite:///:todo.db:')
 Base = declarative_base()
+
+
+class Table(Base):
+    __tablename__ = 'task'
+
+    id = Column(Integer, primary_key=True)
+    name_task = Column(String, default='empty task')
+    details = Column(String, default='empty task details')
+    limit_date = Column(Date, default=datetime.today())
+
+    def __repr__(self) -> str:
+        return f"Table(id={self.id!r}, name={self.name_task!r}, details={self.details!r})"
